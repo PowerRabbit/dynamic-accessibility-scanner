e4cf721c22f95f75f49e044ff65859cbc757d042
Ivan Vlasov <ivan.vlassov@gmail.com>
2025-07-30 16:31:43 +0200

    chore: update settings

--- src/app/components/settings-form/settings-form.component.tsx
+++ src/app/components/settings-form/settings-form.component.tsx
@@ -1,5 +1,16 @@
-import { Button, Field, HStack, Input, Spinner, Stack } from '@chakra-ui/react';
+import {
+    Button,
+    Field,
+    HStack,
+    Input,
+    Spinner,
+    Stack,
+    Portal,
+    Select,
+    createListCollection,
+} from '@chakra-ui/react';
 import { type FC } from 'react';
+import './settings-form.css';
 
 export const SettingsForm: FC<{ submit: () => void, inProgress: boolean }> = ({ submit, inProgress }) => {
 
@@ -13,10 +24,42 @@ export const SettingsForm: FC<{ submit: () => void, inProgress: boolean }> = ({ 
         submit();
     };
 
+    const devices = createListCollection({
+        items: [
+            { label: "No", value: "no" },
+            { label: "iPhone 12 Pro", value: "iphone_12pro" },
+            { label: "Samsung Galaxy S20 Ultra", value: "samsung_galaxy_s20_ultra" },
+        ],
+    })
+
     return (
         <form onSubmit={onSubmit}>
             <h2>View</h2>
             <Stack gap="2" align="flex-start" maxW="sm">
+                <Select.Root collection={devices} defaultValue={["no"]} onValueChange={(e) => {console.log(e.value[0])}}>
+                    <Select.HiddenSelect />
+                        <Select.Label>Device</Select.Label>
+                    <Select.Control>
+                        <Select.Trigger>
+                            <Select.ValueText placeholder="Select virtual device" />
+                        </Select.Trigger>
+                        <Select.IndicatorGroup>
+                            <Select.Indicator />
+                        </Select.IndicatorGroup>
+                    </Select.Control>
+                    <Portal>
+                        <Select.Positioner>
+                            <Select.Content>
+                            {devices.items.map((devices) => (
+                                <Select.Item item={devices} key={devices.value}>
+                                    {devices.label}
+                                    <Select.ItemIndicator />
+                                </Select.Item>
+                            ))}
+                            </Select.Content>
+                        </Select.Positioner>
+                    </Portal>
+                </Select.Root>
                 <Field.Root>
                     <Stack direction={{ base: "column", md: "row" }}>
                             <Field.Label>Height</Field.Label>
